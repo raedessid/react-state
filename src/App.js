95996c6a175e89bb391d21b56f87541da5162129
raed essid <raedessid@isima.u-monastir.tn>
2021-11-07 14:16:12 +0100

    react state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import logo from './logo.svg';
 import './App.css';
+import raed from "./raed.jpg" ;  
+import React, {Component} from 'react' ; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+  
+  constructor(){
+    super() 
+    this.toggleHandler=this.toggleHandler.bind(this) ; 
+  }
+  
+
+
+   state= {
+    fullname : 'Raed Essid' , 
+    bio : 'hard worker' ,  
+    profession :'student' , 
+    imageSrc : raed, 
+    imageAlt :'it\'s my image ' , 
+    show : false , 
+    count: 0,
+   intervalId: 0
+  }; 
+   toggleHandler =() => {
+    const currentState = this.state.show ; 
+    this.setState({
+      show : !currentState  
+    })
+  }
+  componentDidMount() {
+    const newIntervalId = setInterval(() => {
+      this.setState(prevState => {
+        return {
+          ...prevState,
+          count: prevState.count + 1,
+        };
+      });
+    }, 1000);
+  
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        intervalId: newIntervalId,
+      };
+    });
+  }
+  componentDidUpdate () {
+    console.log("component updated")
+  }
+  componentWillUnmount() {
+    console.log("component died") ;
+  }
+  
+shouldComponentUpdate() {
+  return true
+} ;
+
+
+  render() {
+    let content=null ; 
+    if (this.state.show){
+      content = <div>
+      <p className="container"><span> Noun & prenoun  : </span>    {this.state.fullname}</p>
+      <p className="container"><span> Bio : </span>   {this.state.bio}</p>
+      <p className="container"><span>profession : </span>   {this.state.profession} </p>
+      <p><span>profile photo  :   </span></p>
+      <img className="container" src = {this.state.imageSrc} alt = {this.state.imageAlt}/>
+      <p>{this.state.intervalId}</p>
+
+    
+</div>
+    }
+    return (
+      <div>
+        <button  className="btn" onClick={this.toggleHandler} >click me </button>
+        {content}
+      </div>
+    );
+  }
 }
 
 export default App;
